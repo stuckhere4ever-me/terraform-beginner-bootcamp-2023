0106df9eedf14cd1321fa64fb87cfa6daa8fc378
Raza Ali <stuckhere4ever@gmail.com>
2023-09-26 17:52:16 +0000

    #13 Added instructions to create an S3 bucket

--- main.tf
+++ main.tf
@@ -4,22 +4,42 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
   }
+}
+
+provider "aws" {
+  # Configuration options
 }
 
 provider "random" {
   # Configuration options
 }
 
+
 resource "random_string" "bucketname" {
+  # Bucket Naming Rules
+  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
+
+  # Random String Documentation
+  # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
+  
   length           = 16
   special          = false
+  upper            = false
 }
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucketname.result
+
+  }
+ 
 
 output "random_bucket_name" {
-  value       = random_string.bucketname.id
-}
-
-output "random_bucket_result" {
   value       = random_string.bucketname.result
 }
+
